62ad86f59471c79b6172b8fd91e38dd16e4ac9e6
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-27 17:41:42 +0100

    added bookings controller/controller new view/profil controller/profil show/routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :user
+  belongs_to :visitor, class_name: "User"
   belongs_to :flat
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :flats, only: %i[index show create new] do
-    resources :bookings, only: %i[index create new]
+  resources :profils, only: %i[show]
+  resources :bookings, only: %i[index create]
+
+
+  resources :flats, only: %i[index show] do
+    resources :bookings, only: %i[create new]
+  end
+
+  namespace :owner do
+    resources :flats, only: %i[create new]
+    resources :bookings
   end
 end
