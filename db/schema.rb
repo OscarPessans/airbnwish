7a773faff530687d33ce8b37d9b7ab4bcd76695d
Oscar Pessans <oscar.pessans@audencia.com>
2024-02-27 11:49:44 +0100

    rajout des colonnes firstame/lastname/phone à la table user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_150625) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_104811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.text "description"
+    t.string "address"
+    t.float "price"
+    t.integer "size"
+    t.boolean "available"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +35,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_150625) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flats", "users"
 end
