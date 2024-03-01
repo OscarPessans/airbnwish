faea14fb4b17630fdda555627dad4e51d6ae630b
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-03-01 11:30:14 +0100

    formulaire d'ajout de flat marche, on ne peut plus voir nos appartement dans l'index

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,11 @@ class Flat < ApplicationRecord
   has_many_attached :photos, dependent: :destroy
   has_one :booking, dependent: :destroy
   belongs_to :owner, class_name: "User"
+
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :address, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :size, presence: true, numericality: { greater_than: 0 }
+  validates :photos, presence: true
 end
